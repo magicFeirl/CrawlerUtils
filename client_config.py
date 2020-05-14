086caefdc0e2bd9fbbe9fc39a152cb30c5a194fe
magicFeirl <2100709458@qq.com>
2020-05-14 13:16:42 +0800

    新增README; 完善功能;

--- client_config.py
+++ client_config.py
@@ -1,12 +1,66 @@
-from aiohttp import ClientTimeout
+import os
+import json
+
+CONFIG_FILE = 'config.json'
+
+
+def is_dict(obj):
+    return isinstance(obj, dict)
+
+
+def save_config(obj):
+    config = obj
+    if not is_dict(config):
+        config = obj.__dict__
+
+    with open(CONFIG_FILE, 'w') as file:
+        json.dump(config, file)
+        file.flush()
+
+
+def update_config(config, **kwargs):
+    if not is_dict(config):
+        config.__dict__.update(kwargs)
+    else:
+        config.update(kwargs)
+
+
+def get_config():
+    if not os.path.exists(CONFIG_FILE):
+        print('Config does not exist')
+        config = ClientConfig()
+        save_config(config)
+        return config
+    else:
+
+        try:
+            with open(CONFIG_FILE, 'r') as file:
+                config = json.load(file)
+        except IOError as e:
+            print(f'[ERROR] 读取配置文件失败: {e}')
+        except json.decoder.JSONDecodeError as e:
+            print(f'[ERROR] JSON解码失败: {e}')
+        except Exception as e:
+            print(f'[ERROR] 获取配置异常: {e}')
+        else:
+            return ClientConfig(config.get('timeout', 2*60),
+                    config.get('headers', 'wasp'),
+                    config.get('port', ''),
+                    config.get('max_connect_num', 5),
+                    config.get('max_download_num', 100))
+
+        config = ClientConfig()
+        save_config(config)
+        return config
 
 class ClientConfig():
-    def __init__(self, timeout=60, headers=None, port=None,
-    max_connect_num=1, max_download_num=1):
+    def __init__(self, timeout=2*60, headers=None, port='',
+    max_connect_num=5, max_download_num=100):
         self.proxy = None
-        self.timeout = ClientTimeout(total=timeout)
+        self.timeout = timeout
 
         self.headers = headers
+        self.port = port
 
         if not self.headers:
             self.headers = {'User-Agent': 'wasp'}
--- save_files.py
+++ save_files.py
@@ -2,16 +2,18 @@ import os
 
 import aiofiles
 
+CHUNK_SIZE = 64 * 1024
+
 async def save_one(path, file_name, response):
     full_path = os.path.join(path, file_name)
 
     if not os.path.exists(full_path):
         async with aiofiles.open(full_path, 'wb') as f:
             while True:
-                chunk = await response.content.read(64 * 1024)
+                chunk = await response.content.read(CHUNK_SIZE)
                 if not chunk:
                     break
                 await f.write(chunk)
     else:
-        print(full_path, '下已存在同名文件')
+        print(f'\n目标目录下已存在同名文件: {file_name}', end='')
 
